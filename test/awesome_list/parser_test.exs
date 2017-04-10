3f8148970666f33b840cc11262975aa2e81e8edc
Alexander Obukhov <github@sprql.space>
2017-04-10 23:02:19 +0300

    Fix parser test

--- test/awesome_list/parser_test.exs
+++ test/awesome_list/parser_test.exs
@@ -4,17 +4,17 @@ defmodule AwesomeElixir.AwesomeList.ParserTest do
   alias AwesomeElixir.AwesomeList.Parser
 
   setup_all do
-    path = Path.expand("../support/fixtures/awesome_list.md", __DIR__)
+    path = Path.expand("../support/fixtures/awesome_elixir.md", __DIR__)
     list =
       path
       |> File.read!
-      |> Enum.split("\n")
+      |> String.split("\n")
 
-    {:ok, lines}
+    {:ok, list: list}
   end
 
-  test "parse/1 returns parsed awesome list", lines do
-    list = Parser.parse(lines)
+  test "parse/1 returns parsed awesome list", data do
+    list = Parser.parse(data[:list])
     assert Enum.member?(list, {:subsection, "Actors"})
     assert Enum.member?(list, {:subsection_description, "Libraries and tools for working with actors and such."})
     assert Enum.member?(list, {:link, "dflow", "https://github.com/dalmatinerdb/dflow"})
