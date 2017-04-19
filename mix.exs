4ed2d7de373aa77f796ab59419bca929ca727730
Alexander Obukhov <github@sprql.space>
2017-04-19 22:19:04 +0300

    Add tesla lib

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule AwesomeElixir.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:tesla, "~> 0.6.0"},
      {:cowboy, "~> 1.0"}]
   end
 
