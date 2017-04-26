e57c60cdc04de2f16f39702b0b3eabed1c14ba99
Alexander Obukhov <github@sprql.space>
2017-04-26 23:36:33 +0300

    Add update github stats workflow

--- lib/awesome_elixir/awesome_list/workflow.ex
+++ lib/awesome_elixir/awesome_list/workflow.ex
@@ -1,5 +1,6 @@
 defmodule AwesomeElixir.AwesomeList.Workflow do
-  alias AwesomeElixir.AwesomeList.{Parser, Importer}
+  alias AwesomeElixir.AwesomeList
+  alias AwesomeElixir.AwesomeList.{Parser, Importer, GitHub}
 
   def import_awesome_list(url \\ Application.get_env(:awesome_elixir, :awesome_list_url)) do
     response = Tesla.get(url)
@@ -7,5 +8,14 @@ defmodule AwesomeElixir.AwesomeList.Workflow do
     |> String.split("\n")
     |> Parser.parse
     |> Importer.import
+  end
+
+  def update_github_stats do
+    for repository <- AwesomeList.list_repositories, String.match?(repository.url, ~r[^https://github.com]) do
+      with {:ok, attrs} <- GitHub.get_repo_stats(repository.url)
+      do
+        AwesomeList.update_repository(repository, attrs)
+      end
+    end
   end
 end
