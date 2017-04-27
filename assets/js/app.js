0124c3a87884f7ac807cefb9fcc2e7fb5b7cc4ab
Alexander Obukhov <github@sprql.space>
2017-04-27 21:43:39 +0300

    Style awesome list

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,6 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+import timeago from 'timeago.js'
+
+new timeago().render(document.querySelectorAll('[datetime]'));
